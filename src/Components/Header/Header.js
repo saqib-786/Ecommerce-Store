a0fe06be445f41fe034c0e59d3b40ea2a6d43d17
saqib-786 <saqib.qumbrani@gmail.com>
2023-04-03 13:35:46 +0500

    Header and Navbar made responsive

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,32 +1,106 @@
-import React from 'react';
-import { Container } from '@mui/material';
-import {styled} from '@mui/material';
-import { Link } from 'react-router-dom';
-import Nav from '../Nav/Nav';
+import React from "react";
+import { Container, Box } from "@mui/material";
+import { styled } from "@mui/material";
+import { Link } from "react-router-dom";
+import Nav from "../Nav/Nav";
+import { useState } from "react";
+import "../Nav/style.css";
+import { FiShoppingCart } from "react-icons/fi";
 
-const CustomContainer = styled(Container)(({theme,variant,color})=>({
-    backgroundColor:'whitesmoke',
-    display:'flex',
-    justifyContent:'space-between',
-    alignItems:'center',
-    
+const CustomContainer = styled(Container)(({ theme, variant, color }) => ({
+  backgroundColor: "whitesmoke",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+}));
 
-    
-
-}))
+const CustomBox = styled(Box)(({ theme }) => ({
+  width: "100%",
+  height: "90vh",
+  backgroundColor: "white",
+  position: "absolute",
+  zIndex: "5555555",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "0% 0px",
+  justifyContent: "space-evenly",
+}));
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [mobileNav, setMobileNav] = useState(false);
+
   return (
     <React.Fragment>
-
-        <header>
+      <header>
         <CustomContainer maxWidth="">
-            <h1><Link to={'/'}>LOGO</Link></h1>
-            <Nav/>
-        </CustomContainer>
-        </header>
+          <h1>
+            <Link to={"/"}>LOGO</Link>
+          </h1>
 
+          <Nav
+            mobileNav={mobileNav}
+            setMobileNav={setMobileNav}
+            setShowMenu={setShowMenu}
+          />
+        </CustomContainer>
+        {showMenu ? (
+          <CustomBox>
+            <Link
+              onClick={() => {
+                setShowMenu(false);
+                setMobileNav(false);
+              }}
+              to={"/"}
+              className="nav_item"
+            >
+              Home
+            </Link>
+            <Link
+              onClick={() => {
+                setShowMenu(false);
+                setMobileNav(false);
+              }}
+              to={"/about"}
+              className="nav_item"
+            >
+              About
+            </Link>
+            <Link
+              onClick={() => {
+                setShowMenu(false);
+                setMobileNav(false);
+              }}
+              to={"/products"}
+              className="nav_item"
+            >
+              Products
+            </Link>
+            <Link
+              onClick={() => {
+                setShowMenu(false);
+                setMobileNav(false);
+              }}
+              to={"/contact"}
+              className="nav_item"
+            >
+              Contact
+            </Link>
+            <Link
+              onClick={() => {
+                setShowMenu(false);
+                setMobileNav(false);
+              }}
+              to={"/cart"}
+              className="nav_item"
+            >
+              <FiShoppingCart className="cart_2" />
+            </Link>
+          </CustomBox>
+        ) : null}
+      </header>
     </React.Fragment>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,34 +1,52 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import {FiShoppingCart} from 'react-icons/fi'
-import './style.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiShoppingCart, FiMenu } from "react-icons/fi";
+import { Button,Box } from "@mui/material";
+// import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import "./style.css";
 
+function Nav({setShowMenu,mobileNav,setMobileNav}) {
 
-
-function Nav() {
+  
   return (
     <React.Fragment>
-       <nav>
-            <ul className='nav_list'>
-                <li >
-                    <Link className='nav_item'>Home</Link>
-                </li>
-                <li >
-                    <Link className='nav_item'>About</Link>
-                </li>
-                <li >
-                    <Link className='nav_item'>Products</Link>
-                </li>
-                <li >
-                    <Link className='nav_item'>Contact</Link>
-                </li>
-                <li>
-                    <Link><FiShoppingCart className='cart'/></Link>
-                </li>
-            </ul>
-        </nav>
+     <Box>
+     <nav>
+        <ul className="nav_list">
+          <li>
+            <Link to={'/'} className="nav_item">Home</Link>
+          </li>
+          <li>
+            <Link to={'/about'} className="nav_item">About</Link>
+          </li>
+          <li>
+            <Link to={'/products'} className="nav_item">Products</Link>
+          </li>
+          <li>
+            <Link to={'/contact'} className="nav_item">Contact</Link>
+          </li>
+          <li>
+            <Link to={'/cart'}>
+              <FiShoppingCart className="cart" />
+            </Link>
+          </li>
+        </ul>
+
+        {mobileNav ? (
+          <Button onClick={()=>{setMobileNav(!mobileNav);setShowMenu(false)}} className="menu_icon">
+            <IoMdClose className="menu_icon" />
+          </Button>
+        ) : (
+          <Button onClick={()=>{setMobileNav(true);setShowMenu(true)}} className="menu_icon">
+            <FiMenu className="menu_icon" />
+          </Button>
+        )}
+      </nav>
+     </Box>
+      
     </React.Fragment>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
